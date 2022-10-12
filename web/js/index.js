ebdd4f4d8ec48243bf0457383c18277c9757aa93
Juan Rebella <contenedores2040@gmail.com>
2022-10-12 19:53:44 +0100

    Add files via upload

---
+++ web/js/index.js
@@ -0,0 +1,48 @@
+/* 
+  index.js
+*/
+"use strict";
+
+// var msg = "hello JavaScript";
+// console.log(msg);
+
+// var resultsDiv = document.getElementById("results");
+// resultsDiv.innerHTML = "<p>This is from JavaScript</p>";
+
+// var result = {
+//   name: "jQuery",
+//   language: "JavaScript",
+//   score: 4.5,
+//   showLog: function () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
